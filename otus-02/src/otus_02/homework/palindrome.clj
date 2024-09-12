3a719c54970558f7ff7582e13d6940c8cefbc8cf
segi <freeco@mail.ru>
2024-09-12 18:27:03 +0300

    solutions

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -2,5 +2,7 @@
   (:require [clojure.string :as string]))
 
 
-(defn is-palindrome [test-string])
-
+(defn is-palindrome [test-string]
+  (let [original-string (string/lower-case (string/replace test-string #"[^\w]" ""))
+        reversed-string (string/reverse original-string)]
+    (= original-string reversed-string)))
