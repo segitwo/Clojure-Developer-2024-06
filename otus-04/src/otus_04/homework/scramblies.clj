b4906865e48e8324b6988e2a8ddaa389d0be02d1
segi <freeco@mail.ru>
2024-09-13 22:56:52 +0300

    Partial realization

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -3,22 +3,31 @@
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
-(defn scramble?
-  "Функция возвращает true, если из букв в строке letters
-  можно составить слово word."
-  [letters word]
-  nil)
-
 (defn- get-mid [letters]
   (int (/ (count letters) 2)))
 
 (defn- get-char-code-by-idx [letters idx]
   (int (get letters idx)))
 
+(defn scramble?
+  "Функция возвращает true, если из букв в строке letters
+  можно составить слово word."
+  [letters word]
+  (let [word-letters-number (count word)]
+
+    (defn search [letters word-first-letter-code]
+      (let [sorted-deduplicated-letters-mid (get-mid letters)
+            mid-char-code (get-char-code-by-idx letters sorted-deduplicated-letters-mid)]
+        (cond
+          (= word-first-letter-code mid-char-code) true
+          (> word-first-letter-code mid-char-code) (recur (drop sorted-deduplicated-letters-mid letters)
+                                                          word-first-letter-code)
+          (< word-first-letter-code mid-char-code) (recur (take sorted-deduplicated-letters-mid letters)
+                                                          word-first-letter-code))))
+
+    (let [sorted-deduplicated-letters (->> letters (set) (sort) (vec))]
+      (search sorted-deduplicated-letters (int (first word))))))
+
 (comment
-  (def word "world")
-  (let [sorted-deduplicated-letters (->> "helloworld" (set) (sort) (vec))
-        sorted-deduplicated-letters-mid (get-mid sorted-deduplicated-letters)
-        word-first-letter-code (int (first word))
-        mid-char-code (get-char-code-by-idx sorted-deduplicated-letters sorted-deduplicated-letters-mid)]
-    (> word-first-letter-code mid-char-code)))
+  ;;(word-first-letter-code (int (first word)))
+  (scramble? "helloworld" "world"))
